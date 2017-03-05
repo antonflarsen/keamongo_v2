eb8d224562b9b4246d1874bbe81da01cec4be72a
antonflarsen <anto0506@stud.kea.dk>
2017-03-05 12:32:24 +0100

    Geolocation + overlay

--- function.js
+++ function.js
@@ -18,55 +18,86 @@ function initMap() {
 		zoom: 18,
 		center: center,
 	});
-	$.getJSON("database.json", visSteder);
-}
+	//GRÆNSEVÆRDIER TIL OVERLAY
+	var bounds = {
+		north: 55.70732627720079,
+		south: 55.70536769825661,
+		east: 12.540836071441618,
+		west: 12.537359928558317
+	}
 
+	//OVERLAY
+
+	var overlay = new google.maps.GroundOverlay('overlay-01.svg', bounds);
+
+	overlay.setMap(map);
+
+//
+	//GEOLOCATION MARKER
+//
+	if (navigator.geolocation) {
+		navigator.geolocation.watchPosition(function (position) {
+
+			var minPos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+			map.setCenter(minPos);
+
+			var mig = new google.maps.Marker ({
+				position: minPos,
+				icon: 'marker.svg',
+				map: map
+			});
+		});
+	} else {
+		alert("Geolocation failed");
+	}
+//JSON FILEN ER BLEVET KALDT, OG FØRER VIDERE TIL VIS STEDER, SÅ VI KAN TJEKKE OM DEN ER BLEVET HENTET KORREKT.
+	$.getJSON("database.json", visSteder);
+
+}
+//VISER AT JSON FIL ER KALDT OG FØRER VIDERE TIL AT LAVE MARKERS
 function visSteder(listen) {
 	console.log("JSON er kaldt");
 	console.table(listen);
 	listen.forEach(makeMarker);
 }
 
-
+//MARKERS RUNDT OMKRING
 function makeMarker(sted) {
 	console.log("marker er placeret");
-//	visSted(sted);
 
 
 
 
+//DEFINERER MARKER PROPERTIES UD FRA JSON FIL
 	var marker = new google.maps.Marker({
 		position: sted.position,
 		icon: sted.icon,
 		map: map,
-//		icon: 'marker.svg',
+		//		icon: 'marker.svg',
 		title: sted.navn
 	})
-
-marker.addListener("click", clickOnMarker);
+//MARKER KLIKFUNCTION
+	marker.addListener("click", clickOnMarker);
 
 	function clickOnMarker() {
-	console.log("marker er blevet klikket");
-	map.setCenter(sted.position);
+		console.log("marker er blevet klikket");
+//		map.setCenter(sted.position);
+//KLONSTART
+		var klon = document.querySelector("#infoboks").content.cloneNode(true);
 
-	var klon = document.querySelector("#infoboks").content.cloneNode(true);
+		//Sæt data ind i klon -
+		klon.querySelector(".data_navn").textContent = sted.navn;
+		klon.querySelector(".data_adresse").textContent = sted.adresse;
+		klon.querySelector(".data_beskrivelse").textContent = sted.beskrivelse;
+		klon.querySelector(".data_billede img").src = sted.billede;
 
-	//Sæt data ind i klon -
-	klon.querySelector(".data_navn").textContent = sted.navn;
-	klon.querySelector(".data_adresse").textContent = sted.adresse;
-	klon.querySelector(".data_beskrivelse").textContent = sted.beskrivelse;
-	klon.querySelector(".data_billede img").src = sted.billede;
-	klon.querySelector.setAttribute()
 
-	var infowindow = new google.maps.InfoWindow({
-		content: klon
-	})
+		var infowindow = new google.maps.InfoWindow({
+			content: klon
+		})
 
-	infowindow.open(map, marker);
+		infowindow.open(map, marker);
+	}
 }
-}
-
-
-
-
 
